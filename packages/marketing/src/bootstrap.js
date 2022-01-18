4600fd0a963c8b2d40e0c2127ae2feebcbd6421b
Untung <u.untung0029@gmail.com>
2022-01-18 11:30:52 +1100

    changing from browser history to memory history in marketing app

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { createMemoryHistory } from 'history';
 import App from "./App";
 
 const mount = el => {
-  ReactDOM.render(<App />, el);
+  const history = createMemoryHistory();
+  ReactDOM.render(<App history={history}/>, el);
 };
 
 // If we are in development and in isolation, call mount immediately
