c3465f57e0e5f87455225cff62088e0c1691e6cf
Untung <u.untung0029@gmail.com>
2022-01-27 09:35:43 +1100

    Setup the dashboard

---
+++ packages/dashboard/config/webpack.prod.js
@@ -0,0 +1,24 @@
+const { merge } = require("webpack-merge");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const packageJson = require("../package.json");
+const commonConfig = require("../config/webpack.common");
+
+const prodConfig = {
+  mode: "production",
+  output: {
+    filename: "[name].[contenthash].js",
+    publicPath: "/dashboard/latest/",
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "dashboard",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./DashboardApp": "./src/bootstrap.js",
+      },
+      shared: packageJson.dependencies,
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, prodConfig);
---
+++ packages/dashboard/src/bootstrap.js
@@ -0,0 +1,40 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { createMemoryHistory, createBrowserHistory } from "history";
+import App from "./App";
+
+const mount = (el, { onSignIn, initialPath, onNavigate, defaultHistory }) => {
+  const history =
+    defaultHistory ||
+    createMemoryHistory({
+      initialEntries: [initialPath],
+    });
+
+  if (onNavigate) {
+    history.listen(onNavigate);
+  }
+
+  ReactDOM.render(<App onSignIn={onSignIn} history={history} />, el);
+
+  return {
+    onParentNavigate: ({ pathname: nextPathname }) => {
+      const { pathname } = history.location;
+      if (pathname !== nextPathname) {
+        history.push(nextPathname);
+      }
+    },
+  };
+};
+
+// If we are in development and in isolation, call mount immediately
+if (process.env.NODE_ENV === "development") {
+  const devRoot = document.querySelector("#_auth-dev-root");
+  if (devRoot) {
+    mount(devRoot, { defaultHistory: createBrowserHistory() });
+  }
+}
+
+// We are running through container
+// Should export mount function
+
+export { mount };
